17883c3278bd79f1a4d1ac0000e1e2a497f1ae48
Adam Shumann <ashumann13@gmail.com>
2016-02-12 10:18:25 -0600

    Some build changes to better optimize builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,29 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-const browserify = require('gulp-browserify');
- 
+const babelify = require('babelify')
+const watchify = require('watchify')
+const browserify = require('browserify');
+const transform = require('vinyl-transform');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
+const browserifyOpts = {
+	entries: ['./browser.js'],
+	insertGlobals : true,
+	debug : false,
+	standalone: 'ld',
+	transforms: [
+		babelify.configure({
+			// Use all of the ES2015 spec
+			presets: ["es2015"]
+		})
+	]
+}
+
 gulp.task('default', ['node','browser'])
 gulp.task('node', ['es6'])
-gulp.task('browser', ['compress']);
+gulp.task('browser', ['bundle']);
 
 gulp.task('es6', ()=>{
 	return gulp.src('src/**/*.js')
@@ -16,20 +34,14 @@ gulp.task('es6', ()=>{
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('bundle', ['es6'], ()=>{
-	// Single entry point to browserify 
-	return gulp.src('dist/browser.js')
-		.pipe(browserify({
-		  insertGlobals : true,
-		  debug : true,
-		  standalone: 'ld'
-		}))
-		.pipe(gulp.dest('./build'))
-	})
-
-gulp.task('compress', ['bundle'], ()=>{
-	return gulp.src('build/browser.js')
-		.pipe(uglify())
+gulp.task('bundle', [], ()=>{
+	return browserify(browserifyOpts)
+  		.bundle()
+  		.pipe(source('./browser.js'))
+  		.pipe(buffer())
+  		.pipe(rename({ basename: 'node-ld' }))
+  		.pipe(gulp.dest('./build/browser/'))
+  		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('./build'));
-});
+		.pipe(gulp.dest('./build/browser/'));
+})
