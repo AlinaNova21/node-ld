3ca049d62b687bd8d3f387ef8f609def4b3151fc
Michael Studman <mstudman@atlassian.com>
2016-06-15 14:23:46 +0100

    Ensuring hex values are 0 padded

--- samples/characters.js
+++ samples/characters.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var ld = require('..');
 var CharCrypto = ld.CharCrypto;
 var PWDGen = ld.PWDGen;
+var pad = require('pad');
 
 rl = readline.createInterface({
   input: process.stdin,
@@ -18,9 +19,11 @@ rl.question("Enter NFC's UID: ", function(uid) {
     console.log("... [Page  36] [Page  37] ... [Page  43]");
 
     for (character in characters) {
-        console.log("... [" + cc.encrypt(uid, characters[character].id).toString("hex").substring(0, 8)  + "] " +
-            "[" + cc.encrypt(uid, characters[character].id).toString("hex").substring(8, 16)  + "] ... " +
-            "[" + PWDGen(uid).toString(16) + "] " + characters[character].name);
+        var characterCode = pad(16, cc.encrypt(uid, characters[character].id).toString("hex"), '0');
+        var pwd = pad(8, PWDGen(uid).toString(16), '0');
+        console.log("... [" + characterCode.substring(0, 8)  + "] " +
+            "[" + characterCode.substring(8, 16)  + "] ... " +
+            "[" + pwd + "] " + characters[character].name);
     }
   rl.close();
 });
